b541659cf3994298a1ff3c0301b480f41d3f9846
Esteban1211 <spellman3@live.com>
2025-05-01 08:15:32 -0400

    Add project files.

---
+++ Cueva_E_PruebaP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cueva_E_PruebaP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
